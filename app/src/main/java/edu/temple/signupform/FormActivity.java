d98e5430ba42b17cba124f344a45372feba3147c
Jake <tuh13091@temple.edu>
2020-09-23 17:24:14 -0400

    Input logic is complete (make sure all boxes are filled out + check if passwords match)

--- app/src/main/java/edu/temple/signupform/FormActivity.java
+++ app/src/main/java/edu/temple/signupform/FormActivity.java
@@ -1,8 +1,13 @@
 package edu.temple.signupform;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class FormActivity extends AppCompatActivity {
 
@@ -10,5 +15,49 @@ public class FormActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button;
+        button = findViewById(R.id.button);
+
+        myOnclickListener onclickListener = new myOnclickListener();
+        button.setOnClickListener(onclickListener);
+
+    }
+
+    public class myOnclickListener implements View.OnClickListener{
+        @Override
+        public void onClick(View v){
+            EditText et;
+            String[] str = new String[4];
+            et = findViewById(R.id.editTextTextPersonName);
+            str[0] = et.getText().toString();
+            et = findViewById(R.id.editTextTextEmailAddress);
+            str[1] = et.getText().toString();
+            et = findViewById(R.id.editTextTextPassword);
+            str[2] = et.getText().toString();
+            et = findViewById(R.id.editTextTextPassword2);
+            str[3] = et.getText().toString();
+
+            if(str[0].matches("") || str[1].matches("")||str[2].matches("")||str[3].matches("")){
+                displayIncompleteError();
+            }
+            else {
+                if (!str[2].equals(str[3])) {
+                    displayPasswordError();
+                }
+            }
+        }
+        private void displayIncompleteError(){
+            Toast toast = Toast.makeText(getApplicationContext(), "Please Fill Out All Information", Toast.LENGTH_SHORT);
+
+            toast.show();
+        }
+        private void displayPasswordError(){
+            Toast toast = Toast.makeText(getApplicationContext(), "Passwords Do Not Match", Toast.LENGTH_SHORT);
+
+            toast.show();
+        }
+
+
+
     }
 }
