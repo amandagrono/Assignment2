636bc3e1291cbf52439f3b4370a474392710e11a
jacobgronikowski <tuh13091@temple.edu>
2020-09-23 21:01:43 -0400

    Added reset option, to clear the edittext fields

--- app/src/main/java/edu/temple/signupform/FormActivity.java
+++ app/src/main/java/edu/temple/signupform/FormActivity.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class FormActivity extends AppCompatActivity {
@@ -17,10 +19,40 @@ public class FormActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Button button;
         button = findViewById(R.id.button);
+        Button button2;
+        button2 = findViewById(R.id.button2);
 
         myOnclickListener onclickListener = new myOnclickListener();
         button.setOnClickListener(onclickListener);
 
+        resetOnClickListener reset = new resetOnClickListener();
+        button2.setOnClickListener(reset);
+
+
+    }
+    public class resetOnClickListener implements View.OnClickListener{
+
+        @Override
+        public void onClick(View v){
+            TextView tv = findViewById(R.id.textView);
+            tv.setText("Please Enter Your Information");
+            tv.setGravity(Gravity.CENTER_HORIZONTAL);
+            Button button = findViewById(R.id.button2);
+            button.setVisibility(Button.INVISIBLE);
+            button = findViewById(R.id.button);
+            button.setVisibility(Button.VISIBLE);
+
+            EditText et = findViewById(R.id.editTextTextPersonName);
+            et.getText().clear();
+            et = findViewById(R.id.editTextTextEmailAddress);
+            et.getText().clear();
+            et = findViewById(R.id.editTextTextPassword);
+            et.getText().clear();
+            et = findViewById(R.id.editTextTextPassword2);
+            et.getText().clear();
+
+
+        }
     }
 
     public class myOnclickListener implements View.OnClickListener{
@@ -44,6 +76,9 @@ public class FormActivity extends AppCompatActivity {
                 if (!str[2].equals(str[3])) {
                     displayPasswordError();
                 }
+                else{
+                    messageToUser(str[0]);
+                }
             }
         }
         private void displayIncompleteError(){
@@ -55,6 +90,16 @@ public class FormActivity extends AppCompatActivity {
             Toast toast = Toast.makeText(getApplicationContext(), "Passwords Do Not Match", Toast.LENGTH_SHORT);
 
             toast.show();
+        }
+        private void messageToUser(String name){
+            TextView tv = findViewById(R.id.textView);
+            tv.setText("Hello "+name+ ", welcome to the SignUpForm app");
+            tv.setGravity(Gravity.CENTER_HORIZONTAL);
+            Button button = findViewById(R.id.button);
+            button.setVisibility(Button.INVISIBLE);
+            button = findViewById(R.id.button2);
+            button.setVisibility(Button.VISIBLE);
+
         }
 
 
